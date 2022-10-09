e229f2e4a2ba8a335779e851faf40f80795c14a6
Ezequiel Duarte <ezeqduarte01@gmail.com>
2022-10-08 23:52:54 -0300

    cambios en cards

--- index.js
+++ index.js
@@ -12,29 +12,30 @@ function sprintCards(events, container) {
     container.innerHTML += `
     
     <div class="card card_events p-2">
-    <div class="pic_event_card d-flex align-items-center justify-content-center">
-    <img src="${event.image}" alt="${event.image}">
-    <h5 class="card-title text-center">${event.name}</h5>
+      <div class="pic_event_card d-flex align-items-center justify-content-center">
+          <img src="${event.image}" alt="${event.image}">
+          <h5 class="card-title text-center">${event.name}</h5>
+      </div>
+      <div class="card-body d-flex flex-column justify-content-between">
+          <p class="card-text text-center texto-cards">${event.description}</p>
+          <p class="card-text">
+              <span class="datos-cards">Date:</span>
+              <span class="datos-datos-cards">${event.date}</span>
+          </p>
+          <p class="card-text">
+              <span class="datos-cards">Category:</span>
+              <span class="datos-datos-cards">${event.category}</span>
+          </p>                                      
+          <p class="card-text">
+            <span class="datos-cards">Price:</span>
+            <span class="datos-datos-cards"> $ ${event.price}</span>
+          </p>
+      </div>  
+      <div class="d-flex justify-content-end">
+          <a href="details.html" class="btn btn-primary">More information</a>
+      </div>                  
     </div>
-    <div class="card-body d-flex flex-column justify-content-between">
-    <p class="card-text text-center texto-cards">${event.description}</p>
-    <p class="card-text">
-    <span class="datos-cards">Date:</span>
-    <span class="datos-datos-cards">${event.date}</span>
-    </p>
-    <p class="card-text">
-    <span class="datos-cards">Category:</span>
-    <span class="datos-datos-cards">${event.category}</span>
-    </p>                                      
-    <p class="card-text">
-    <span class="datos-cards">Price:</span>
-    <span class="datos-datos-cards"> $ ${event.price}</span>
-    </p>
-    </div>  
-    <div class="d-flex justify-content-end">
-    <a href="details.html" class="btn btn-primary">More information</a>
-    </div>                  
-    </div>
+    
     `;
   }
 }
